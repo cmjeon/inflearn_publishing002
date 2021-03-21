dbb78d637d519b2e31ab94db2eefc1795fb145d9
cmj <chmin82@gmail.com>
2021-03-21 17:15:21 +0900

    scroll animation 제작

--- js/script.js
+++ js/script.js
@@ -14,3 +14,39 @@ $(function() {
     $('.gnb').removeClass('on');
   })
 });
+
+//fixHeader
+var scrollTop=0;
+scrollTop=$(document).scrollTop();
+console.log(scrollTop);
+fixHeader();
+
+//윈도우창 조절시 이벤트
+$(window).on('scroll resize', function() {
+  scrollTop=$(document).scrollTop();
+  fixHeader();
+});
+
+//고정헤더 함수
+function fixHeader() {
+  if(scrollTop > 150) {
+    $('header').addClass('on');
+  } else {
+    $('header').removeClass('on');
+  }
+}
+
+//스크롤 애니메이션
+$(function() {
+  $('.animate').scrolla({
+    mobile:false,
+    once:false,
+  });
+});
+
+//상단으로 부드럽게
+$(function() {
+  $('.goTop').on('click', function() {
+    $('html, body').animate({ scrollTop:0 }, 400 );
+  });
+});
